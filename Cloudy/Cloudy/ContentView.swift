ef5f358a6c324c57b588a3c92c8fafb502dcada1
Pedro da Matta <pmrm@cin.ufpe.br>
2021-03-11 09:42:10 -0300

    Initial xcode commit

---
+++ Cloudy/Cloudy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cloudy
+//
+//  Created by Pedro Moura on 11/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Cloudy!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
