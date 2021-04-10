2f95c7156830261f7cc1120accc6ed47b0d6b004
Juliano Cezar Teles Vaz <julianoctvaz@gmail.com>
2021-04-09 21:58:38 -0300

    Create service from CoreData to store completed pauses

--- Cloudy/Cloudy/Services/DataManager.swift
+++ Cloudy/Cloudy/Services/DataManager.swift
@@ -19,6 +19,8 @@ class DataManager {
         self.managedContext = moc
     }
     
+    //MARK: Pause fuctions
+    
     // get records
     func getPauses() -> [Pause] {
         var pauses = [Pause]()
@@ -74,6 +76,71 @@ class DataManager {
             print(error)
         }
     }
+    
+    //MARK: CompletedPause functions
+    
+    
+    // get records
+    func getCompletedPauses() -> [CompletedPause] {
+        var CompletedPauses = [CompletedPause]()
+        let bdRequest: NSFetchRequest<CompletedPause> = CompletedPause.fetchRequest()
+        
+        do {
+            CompletedPauses = try self.managedContext.fetch(bdRequest)
+        } catch {
+            print(error)
+        }
+        
+        return CompletedPauses
+    }
+    
+    // save pause
+    func saveCompletedPause(id: UUID, endDate: Date, expectedEndDate: Date, feelingAfterPause: String, startDate: Date) {
+        let cp = CompletedPause(context: self.managedContext)
+        cp.id = id
+        cp.endDate = endDate
+        cp.expectedEndDate = expectedEndDate
+        cp.feelingAfterPause = feelingAfterPause
+        cp.startDate = startDate
+        do {
+            try self.managedContext.save()
+        } catch {
+            print(error)
+        }
+    }
+    
+    // remove pause
+    func removeCompletedPause(id: UUID) {
+        let fetchRequest: NSFetchRequest<CompletedPause> = CompletedPause.fetchRequest()
+        fetchRequest.predicate = NSPredicate.init(format: "id=%@", id.uuidString)
+        do {
+            let cps = try self.managedContext.fetch(fetchRequest)
+            for cp in cps {
+                self.managedContext.delete(cp)
+            }
+            try self.managedContext.save()
+        } catch {
+            print(error)
+        }
+    }
+    
+    // update pause
+    func updatePause(id: UUID, endDate: Date, expectedEndDate: Date, feelingAfterPause: String, startDate: Date) {
+        let fetchRequest: NSFetchRequest<CompletedPause> = CompletedPause.fetchRequest()
+        fetchRequest.predicate = NSPredicate.init(format: "id=%@", id.uuidString)
+        do {
+            let cp = try self.managedContext.fetch(fetchRequest).first
+            cp?.endDate = endDate
+            cp?.expectedEndDate = expectedEndDate
+            cp?.feelingAfterPause = feelingAfterPause
+            cp?.startDate = startDate
+            try self.managedContext.save()
+        } catch {
+            print(error)
+        }
+    }
+
+    
 }
 
 extension NSManagedObjectContext {
